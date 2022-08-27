d12c713fc5cb6fde4cac1f22cdc4b4f77bb71a2b
zargham-i <zargham.ibrahim1@gmail.com>
2022-08-27 18:33:09 -0400

    Initial commit

---
+++ src/main/java/com/web/x1/ProjectX1Application.java
@@ -0,0 +1,13 @@
+package com.web.x1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectX1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectX1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/x1/ProjectX1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.x1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectX1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
