56b733be9d89a9af2ef96f17481026196cf923f8
zargham-i <zargham.ibrahim1@gmail.com>
2022-08-27 21:35:22 -0400

    File name update

--- src/main/java/com/web/x1/MovieSearchApplication.java
+++ src/main/java/com/web/x1/MovieSearchApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieSearchApiApplication {
+public class MovieSearchApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MovieSearchApiApplication.class, args);
+        SpringApplication.run(MovieSearchApplication.class, args);
     }
 }
